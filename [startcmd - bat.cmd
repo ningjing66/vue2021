109cf12f1de21612b21503b351be9fdb87c02918
ningjing66 <syw99@163.com>
2021-03-22 22:16:20 +0800

    msg

---
+++ [startcmd - bat.cmd
@@ -0,0 +1,12 @@
+@echo off
+color a
+@start
+exit
+
+
+@rem echo on @cmd  @python aa1.py  @python aa01.py
+@python [starttestpython.py
+@rem echo on @cmd  @python aa1.py  @python aa11.py
+
+@rem powershell
+@start
